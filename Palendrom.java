bd74e84633f7ab018aa095ad175937cb0ca84d8a
rohit <rohitgiri00@yahoo.com>
2023-01-09 20:43:27 +0530

    adding palendrome file

--- Palendrom.java
+++ Palendrom.java
@@ -4,10 +4,20 @@ import java.util.Scanner;
 public class Palendrom {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.print("enter String : ");
         String String = input.next();
 
-        for (int i=0;i<String.length()/2;i++){
-            String.compareTo(i);
+        String rev="";
+        for (int i=String.length()-1;i>=0;i--){
+            rev=rev+String.charAt(i);
         }
+
+        if (String.equals(rev)){
+            System.out.println("given String is palindrome");
+        }
+        else {
+            System.out.println("given String is not palindrome");
+        }
+
     }
 }
